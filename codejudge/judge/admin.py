6b985ee3916434650a9160a2c34a1e3f440eabef
Jinank Jain <ug201210017@iitj.ac.in>
2015-03-30 01:24:32 +0530

    Admin View updated

--- codejudge/judge/admin.py
+++ codejudge/judge/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from judge.models import *
 
 # Register your models here.
+admin.site.register(Hacker)
+admin.site.register(Problem)
+admin.site.register(Contest)
+admin.site.register(Language)
+admin.site.register(Solution)
+admin.site.register(Comments)
